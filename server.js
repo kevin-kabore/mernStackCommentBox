b7d99de4e8dcd9bd8091f2f514d20b7c5c14d9f5
Kevin Kabore <kevin.s.kabore@gmail.com>
2017-08-10 16:22:13 -0700

    adds get and post method

--- server.js
+++ server.js
@@ -41,6 +41,33 @@ router.get('/', function(req, res) {
   res.json({message: 'API initialized'});
 })
 
+// add /comments route to /api router
+router.route('/comments')
+  // retrieve all comment from the database
+  .get(function(req, res) {
+    //look at Comment schema
+    Comment.find(function(err, comments){
+      if(err) {
+        res.send(err);
+      }
+      // res with json object of database comments
+      res.json(comments)
+    });
+  })
+  // post new comment to the database
+  .post(function(req, res) {
+    var comment = new Comment();
+    // body-parser lets us use req.body
+    comment.author = req.body.author;
+    comment.text = req.body.text;
+
+    comment.save(function(err) {
+      if(err) {
+        res.send(err)
+      }
+      res.json({message: 'Comment successfully added!'});
+    })
+  })
 // use router configuration when we call /api
 app.use('/api', router);
 

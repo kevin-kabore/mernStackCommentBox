60db29d6826ca2ce47d151483a271ac8b5cfab19
Kevin Kabore <kevin.s.kabore@gmail.com>
2017-08-10 11:54:37 -0700

    initializes API

---
+++ server.js
@@ -0,0 +1,42 @@
+'use strict'
+
+// import dependencies
+var express = require('express');
+var mongoose = require('mongoose');
+var bodyParser = require('body-parser');
+
+// and create instances
+var app = express();
+var router = express.Router();
+
+// set port to either predetermined port or 30001
+var port = process.env.API_PORT || 3001;
+
+// configure API to use bodyParser and look for JSON data in request body
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.json());
+
+// To prevent errors from Cross Origin Resource Sharing, set headers to allow CORS with middleware:
+app.use(function(req, res, next) {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Credentials', 'true');
+  res.setHeader('Access-Control-Allow-Methods', 'GET,HEAD,OPTIONS,POST,PUT,DELETE');
+  res.setHeader('Access-Control-Allow-Headers', 'Access-Control-Allow-Headers, Origin,Accept, X-Requested-With, Content-Type Access-Control-Request-Method, Access-Control-Request-Headers');
+
+  // remove cacheing to get most recent comments
+  res.setHeader('Cache-Control', 'no-cache');
+  next();
+});
+
+// set up route path & initialize the API
+router.get('/', function(req, res) {
+  res.json({message: 'API initialized'});
+})
+
+// use router configuration when we call /api
+app.use('/api', router);
+
+// start the server and listen for requests
+app.listen(port, function() {
+  console.log(`api running on port ${port}`);
+})
